baabb1a4eb68f169326c2f6c406b98771df521c9
Elinmaculado <miguel.ruiz@uniat.edu.mx>
2023-03-23 10:52:16 -0600

    switch

--- Source.cpp
+++ Source.cpp
@@ -4,5 +4,15 @@
 using namespace std;
 
 int main() {
+	int option;
+	
+	cout << "elija el numero de examen que desea ver: ";
+	cin >> option;
 
+	switch (option) {
+	case 1:
+		break;
+	case 2:
+		cout << "el examen 2 aun no existe xd";
+	}
 }
